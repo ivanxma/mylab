834f4fdfa4405f4c829f085b11c2c83c6970d0cc
MySQL Server <mysql@instance-20210119-1947.sub06260250450.mysqlvcn.oraclevcn.com>
2021-01-19 14:20:51 +0000

    8.0.23 change

--- 13-InnoDBCluster/06-bridgeServer/21-addSourceList.sh
+++ 13-InnoDBCluster/06-bridgeServer/21-addSourceList.sh
@@ -1,27 +1,44 @@
 . ./comm.sh
 
-mysql -uroot -h127.0.0.1 -P3306 << EOL
+mysqlsh --uri gradmin:grpass@$CLUSTER_HOST:3310 << EOL
 
-set global super_read_only=0;
-set sql_log_bin=0;
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3310, '', 50);
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3320, '', 80);
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3330, '', 90);
+var v = session.runSql('select @@group_replication_group_name;').fetchAll();
+var gname = v[0][0];
 
+print("Group Replication Grouop Name : " + gname + "\n");
+
+var sess3306 = shell.connect("root:@127.0.0.1:3306")
+sess3306.runSql(" set global super_read_only=0; ")
+sess3306.runSql(" set sql_log_bin=0; ")
+
+sess3306.runSql(" SELECT asynchronous_connection_failover_delete_managed('$BRIDGE_CHANNEL', '" + gname + "');")
+
+sess3306.runSql("SELECT asynchronous_connection_failover_add_managed('$BRIDGE_CHANNEL', 'GroupReplication', '" + gname + "', '$CLUSTER_HOST', 3310, '', 80, 60); ")
+sess3306.runSql("SELECT asynchronous_connection_failover_add_managed('$BRIDGE_CHANNEL', 'GroupReplication', '" + gname + "', '$CLUSTER_HOST', 3320, '', 80, 60); ")
+sess3306.runSql("SELECT asynchronous_connection_failover_add_managed('$BRIDGE_CHANNEL', 'GroupReplication', '" + gname + "', '$CLUSTER_HOST', 3330, '', 80, 60); ")
+
+// sess3306.runSql(" SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3320, '', 90);")
+// sess3306.runSql(" SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3330, '', 80);")
+EOL
+
+
+
+mysql -t -uroot -h127.0.0.1 -P3306 << EOL
+
+SELECT * FROM performance_schema.replication_asynchronous_connection_failover_managed;
 SELECT * FROM performance_schema.replication_asynchronous_connection_failover;
-
 
 EOL
 
-mysql -uroot -h127.0.0.1 -P3316 << EOL
+#mysql -uroot -h127.0.0.1 -P3316 << EOL
 
-set global super_read_only=0;
-set sql_log_bin=0;
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3310, '', 50);
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3320, '', 90);
-SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3330, '', 80);
+#set global super_read_only=0;
+#set sql_log_bin=0;
+#SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3310, '', 50);
+#SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3320, '', 90);
+#SELECT asynchronous_connection_failover_add_source('$BRIDGE_CHANNEL', '$CLUSTER_HOST', 3330, '', 80);
+#
+#SELECT * FROM performance_schema.replication_asynchronous_connection_failover;
+#
 
-SELECT * FROM performance_schema.replication_asynchronous_connection_failover;
-
-
-EOL
+#EOL
--- 13-InnoDBCluster/06-bridgeServer/22-addReplicationChannel.sh
+++ 13-InnoDBCluster/06-bridgeServer/22-addReplicationChannel.sh
@@ -5,20 +5,20 @@
 # persist REPLICATION FILTER in config file
 #
 #    SOURCE_CONNECTION_AUTO_FAILOVER : activates the asynchronous connection failover mechanism.
-#    MASTER_RETRY_COUNT & MASTER_CONNECT_RETRY : define the failover time. The default setting is… 60 days, probably not what you want :). So, you (most likely) should reduced the settings. e.g. 1 minute is respectively 20 and 3. (20 x 3 = 60)
-#     FOR CHANNEL : enables you to name which replication channel the statement applies to. The CHANGE MASTER TO statement applies to this specific replication channel.
+#    SOURCE_RETRY_COUNT & SOURCE_CONNECT_RETRY : define the failover time. The default setting is… 60 days, probably not what you want :). So, you (most likely) should reduced the settings. e.g. 1 minute is respectively 20 and 3. (20 x 3 = 60)
+#     FOR CHANNEL : enables you to name which replication channel the statement applies to. The CHANGE REPLICATION SOURCE TO statement applies to this specific replication channel.
 
 mysql -uroot -h127.0.0.1 -P3306  << EOF
 
-CHANGE MASTER TO
+CHANGE REPLICATION SOURCE TO
   master_host = '$CLUSTER_HOST',
   master_port = 3330,
   master_user = 'repl',
   master_password = 'repl',
   master_auto_position=1,
   SOURCE_CONNECTION_AUTO_FAILOVER=1, 
-  MASTER_RETRY_COUNT=3, 
-  MASTER_CONNECT_RETRY=5 
+  SOURCE_RETRY_COUNT=3, 
+  SOURCE_CONNECT_RETRY=5 
 FOR CHANNEL '$BRIDGE_CHANNEL';
 
 CHANGE REPLICATION FILTER
@@ -34,15 +34,15 @@ EOF
 
 mysql -uroot -h127.0.0.1 -P3316  << EOF
 
-CHANGE MASTER TO
+CHANGE REPLICATION SOURCE TO
   master_host = '$CLUSTER_HOST',
   master_port = 3320,
   master_user = 'repl',
   master_password = 'repl',
   master_auto_position=1,
   SOURCE_CONNECTION_AUTO_FAILOVER=1, 
-  MASTER_RETRY_COUNT=3, 
-  MASTER_CONNECT_RETRY=5 
+  SOURCE_RETRY_COUNT=3, 
+  SOURCE_CONNECT_RETRY=5 
 FOR CHANNEL '$BRIDGE_CHANNEL';
 
 CHANGE REPLICATION FILTER
