69049c279557f875539ee566fe90a0cc3ef2dcf5
MySQL Server <mysql@test01.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-03-30 11:06:26 +0000

    updated

--- comm.sh
+++ comm.sh
@@ -4,12 +4,12 @@ export ROUTER=/usr/local/router
 export PATH=$MYSQL_HOME/bin:$SHELL_HOME/bin:$ROUTER/bin:$PATH
 export LD_LIBRARY_PATH=$ROUTER/lib:$ROUTER/lib/mysqlrouter:$LD_LIBRARY_PATH
 
-export BRIDGE_HOST=ivanma-demo2
+export BRIDGE_HOST=$HOSTNAME
 export BRIDGE_CHANNEL=channel1
 export BRIDGE_DRCHANNEL=drchannel1
-export CLUSTER_HOST=ivanma-demo1
+export CLUSTER_HOST=$HOSTNAME
 export CLUSTER_IPALLOWLIST=10.0.0.0/16
-export SECONDARY_HOST=ivanma-demo3
+export SECONDARY_HOST=$HOSTNAME
 
 if [ ! -r config/gencnf.touch ]
 then
