98400b95a3ad5b0ad8c69a8f9d59134637ed1e7a
MySQL Server <mysql@test01.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-04-01 03:58:34 +0000

    add mkdir

---
+++ cluster/01-init/00-mkdir.sh
@@ -0,0 +1,6 @@
+mkdir -p /home/mysql/mycluster/
+mkdir -p /home/mysql/mycluster/mgm01/ndb_data
+mkdir -p /home/mysql/mycluster/mgm02/ndb_data
+mkdir -p /home/mysql/mycluster/ndb01/ndb_data
+mkdir -p /home/mysql/mycluster/ndb02/ndb_data
+mkdir -p /home/mysql/mycluster/mysql150/mysql_data
