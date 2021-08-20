b595361336cf22da1025f9d27423d5ebbac0e8dd
MySQL Server <mysql@my8026a-667229.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-08-20 00:03:05 +0000

    updated

--- 04-MEB/04-innodb-meb/43-createInnoDBCluster.sh
+++ 04-MEB/04-innodb-meb/43-createInnoDBCluster.sh
@@ -4,7 +4,7 @@ mysqlsh --uri gradmin:grpass@${PRIMARY_HOST}:3306 << EOL
 dba.createCluster('mycluster')
 var x = dba.getCluster('mycluster')
 
-x.addInstance('gradmin:grpass@{PRIMARY_HOST}:3316')
-x.addInstance('gradmin:grpass@{PRIMARY_HOST}:3326')
+x.addInstance('gradmin:grpass@${PRIMARY_HOST}:3316')
+x.addInstance('gradmin:grpass@${PRIMARY_HOST}:3326')
 print(x.status())
 EOL
