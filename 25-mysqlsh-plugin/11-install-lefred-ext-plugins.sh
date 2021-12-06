aa827d7950fdb89baf0c232c5969ca1308197ec4
MySQL Server <mysql@myinstance8027.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-12-06 03:01:25 +0000

    update

--- 25-mysqlsh-plugin/11-install-lefred-ext-plugins.sh
+++ 25-mysqlsh-plugin/11-install-lefred-ext-plugins.sh
@@ -1,4 +1,5 @@
 mkdir -p ~/.mysqlsh/plugins/
+curl https://codeload.github.com/lefred/mysqlshell-plugins/zip/refs/heads/master > master.zip
 unzip master.zip -d ~/.mysqlsh/plugins/
 mv ~/.mysqlsh/plugins/mysqlshell-plugins-master/* ~/.mysqlsh/plugins/
 
