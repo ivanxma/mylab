bbea16d9c60ce22c31695ab0c4bc60c71f9ae1e0
MySQL Server <mysql@myinstance8027.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-12-10 03:03:13 +0000

    add unset

--- 25-mysqlsh-plugin/plugins/heatwave/init.py
+++ 25-mysqlsh-plugin/plugins/heatwave/init.py
@@ -7,9 +7,33 @@ class heatwave_utils:
     A collection of utils to manage heatwavse
     """
 
+def __returnSecEngineTables(session, schema):
+   
+    # Define the query to get the routines
+    stmt = "select table_schema, table_name, create_options from information_schema.tables t where create_options like '%SECONDARY_ENGINE=%RAPID%'"
+
+    if schema is not None:
+       stmt  = stmt + " AND t.table_schema='%s'" % schema
 
 
-def __returnTables(session, schema):
+    stmt = stmt + ";"
+    # Execute the query and check for warnings
+    result = session.run_sql(stmt)
+    tables = result.fetch_all()
+    if (result.get_warnings_count() > 0):
+        # Bail out and print the warnings
+        print("Warnings occurred - bailing out:")
+        print(result.get_warnings())
+        return False
+    tablearray = []
+    for row in tables:
+        tablearray.append( row[1] )
+
+    return tablearray;
+
+
+
+def __returnSecLoadedTables(session, schema):
    
     # Define the query to get the routines
 #    stmt = "select concat(t.table_schema, '.', t.table_name) from information_schema.tables t where t.table_type = 'BASE TABLE' "
@@ -34,8 +58,8 @@ def __returnTables(session, schema):
     return tablearray;
 
 
-@plugin_function("heatwave_utils.list_tables")
-def list_tables(schema=None, session=None):
+@plugin_function("heatwave_utils.list_secondary_loaded_tables")
+def list_sec_loaded_tables(schema=None, session=None):
     """
     Wizard to unload secondary_engine for tables in schema
 
@@ -61,7 +85,38 @@ def list_tables(schema=None, session=None):
 	
     db = session.get_schema(schema)
 
-    tables = __returnTables(session, schema)
+    tables = __returnSecLoadedTables(session, schema)
+
+    return tables;
+
+@plugin_function("heatwave_utils.list_secondary_engine_tables")
+def list_sec_engine_tables(schema=None, session=None):
+    """
+    Wizard to unload secondary_engine for tables in schema
+
+    Args:
+        schema (string): The session to be used on the operation.
+        session (object): The optional session object
+
+    """
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+
+    if schema is None:
+        print("No schema specified.")
+        return
+	
+    db = session.get_schema(schema)
+
+    tables = __returnSecEngineTables(session, schema)
 
     return tables;
 
@@ -94,10 +149,48 @@ def unload_schema(schema=None, session=None):
 	
     db = session.get_schema(schema)
     session.set_current_schema(schema)
-    table = __returnTables(session, schema)
+    table = __returnSecLoadedTables(session, schema)
     for i in table:
         print('alter table ' + i + ' secondary_unload')
         result = session.run_sql('alter table ' + i + ' secondary_unload')
+        shell.dump_rows(result)
+
+
+    return
+
+
+@plugin_function("heatwave_utils.unset_schema")
+def unload_schema(schema=None, session=None):
+    """
+    Wizard to unset secondary_engine for tables in schema
+
+    Args:
+        schema (string): The session to be used on the operation.
+        session (object): The optional session object
+
+    """
+
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+
+    if schema is None:
+        print("No schema specified.")
+        return
+	
+    db = session.get_schema(schema)
+    session.set_current_schema(schema)
+    table = __returnSecEngineTables(session, schema)
+    for i in table:
+        print('alter table ' + i + ' secondary_engine=null')
+        result = session.run_sql('alter table ' + i + ' secondary_engine=null')
         shell.dump_rows(result)
 
 
