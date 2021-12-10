286858b2d42c6cc2956f4f9417663de46f32d6a4
MySQL Server <mysql@myinstance8027.sub07160345580.mysqlvcnivanma.oraclevcn.com>
2021-12-10 07:34:58 +0000

    updated heatwave_utils

--- 25-mysqlsh-plugin/plugins/heatwave_utils/init.py
+++ 25-mysqlsh-plugin/plugins/heatwave_utils/init.py
@@ -2,10 +2,40 @@ from mysqlsh.plugin_manager import plugin, plugin_function
 @plugin
 class heatwave_utils:
     """
-    Heatwavse Utils 
+    Heatwave Utils 
 
     A collection of utils to manage heatwavse
     """
+
+def __runAndReturn(session, sqltext=None) :
+    if sqltext is not None:
+        stmt = sqltext;
+
+
+
+    stmt = stmt + ";"
+    # Execute the query and check for warnings
+    result = session.run_sql(stmt)
+    if (result.get_warnings_count() > 0):
+        # Bail out and print the warnings
+        print("Warnings occurred - bailing out:")
+        print(result.get_warnings())
+        return False
+
+    return result;
+
+def __loadSecTables(session, schema):
+   
+    # Define the query to get the routines
+    if schema is not None:
+        stmt = "call sys.heatwave_load(JSON_ARRAY('%s'), null)" % schema
+        stmt = stmt + ";"
+
+
+    # Execute the query and check for warnings
+    result = __runAndReturn(session, stmt)
+
+    return result;
 
 def __returnSecEngineTables(session, schema):
    
@@ -30,7 +60,6 @@ def __returnSecEngineTables(session, schema):
         tablearray.append( row[1] )
 
     return tablearray;
-
 
 
 def __returnSecLoadedTables(session, schema):
@@ -61,7 +90,7 @@ def __returnSecLoadedTables(session, schema):
 @plugin_function("heatwave_utils.list_secondary_loaded_tables")
 def list_sec_loaded_tables(schema=None, session=None):
     """
-    Wizard to unload secondary_engine for tables in schema
+    Wizard to list secondary engine with data loaded tables 
 
     Args:
         schema (string): The session to be used on the operation.
@@ -92,7 +121,7 @@ def list_sec_loaded_tables(schema=None, session=None):
 @plugin_function("heatwave_utils.list_secondary_engine_tables")
 def list_sec_engine_tables(schema=None, session=None):
     """
-    Wizard to unload secondary_engine for tables in schema
+    Wizard to list tables with secondary engine
 
     Args:
         schema (string): The session to be used on the operation.
@@ -160,7 +189,7 @@ def unload_schema(schema=None, session=None):
 
 
 @plugin_function("heatwave_utils.unset_schema")
-def unload_schema(schema=None, session=None):
+def unset_schema(schema=None, session=None):
     """
     Wizard to unset secondary_engine for tables in schema
 
@@ -195,3 +224,91 @@ def unload_schema(schema=None, session=None):
 
 
     return
+
+@plugin_function("heatwave_utils.load_schema")
+def load_schema(schema=None, session=None):
+    """
+    Wizard to unset secondary_engine for tables in schema
+
+    Args:
+        schema (string): The session to be used on the operation.
+        session (object): The optional session object
+
+    """
+
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+
+    if schema is None:
+        print("No schema specified.")
+        return
+	
+    db = session.get_schema(schema)
+    session.set_current_schema(schema)
+    result = __loadSecTables(session, schema)
+    shell.dump_rows(result)
+
+    return
+
+@plugin_function("heatwave_utils.set_trace_on")
+def set_trace_on( session=None):
+    """
+    Wizard to set trace on
+
+    Args:
+        session (object): The optional session object
+
+    """
+
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+
+    result = __runAndReturn(session, "SET SESSION optimizer_trace='enabled=on';")
+    result = __runAndReturn(session, "SET optimizer_trace_offset=-2;")
+    shell.dump_rows(result)
+
+    return
+
+@plugin_function("heatwave_utils.set_trace_off")
+def set_trace_off( session=None):
+    """
+    Wizard to set trace off
+
+    Args:
+        session (object): The optional session object
+
+    """
+
+    # Get hold of the global shell object
+    import mysqlsh
+    shell = mysqlsh.globals.shell
+
+    if session is None:
+        session = shell.get_session()
+        if session is None:
+            print("No session specified. Either pass a session object to this "
+                  "function or connect the shell to a database")
+            return
+
+    result = __runAndReturn(session, "SET SESSION optimizer_trace='enabled=off';")
+    shell.dump_rows(result)
+
+    return
+
+from heatwave_utils import reports
