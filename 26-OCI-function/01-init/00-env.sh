a71705631c95ee70eb8e25f75d5a18c4790eb2d5
ivanxma <ivanxma@yahoo.com>
2022-09-28 17:26:44 +0800

    add env

---
+++ 26-OCI-function/01-init/00-env.sh
@@ -0,0 +1,11 @@
+# Create the compartment
+# Create VCN and subnet 1/2 =private and public
+
+COMPARTMENT_ID=
+SUBNET1_ID=
+SUBNET2_ID=ocid1.subnet.oc1.
+
+if [ -r ./00-env.sh.ignore ]
+then
+. ./00-env.sh.ignore
+fi
