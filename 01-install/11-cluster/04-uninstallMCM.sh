dccd85c43adbd79bcc5855936c1f0d1d46220d2e
Ivan Ma <ivanxma@yahoo.com>
2022-10-12 05:59:08 +0000

    change with 8.0.31

--- 01-install/11-cluster/04-uninstallMCM.sh
+++ 01-install/11-cluster/04-uninstallMCM.sh
@@ -1,6 +1,6 @@
-MCMFOLDER=/home/mysql/mcm/mcm1.4.8
+MCMFOLDER=/home/mysql/mcm/mcm8.0.31
 MCMLINK=/home/mysql/mcm/mcm-latest
-MCMTAR=/opt/download/mysql/mcm/mcm-1.4.8-linux-glibc2.12-x86-64bit.tar.gz
+MCMTAR=/opt/download/mysql/mcm/mcm-8.0.31-linux-el8-x86-64bit.tar.gz
 
 if [ $USER != 'root' ]
 then
