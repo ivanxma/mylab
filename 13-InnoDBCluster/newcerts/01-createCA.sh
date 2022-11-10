dc3ec3e94c791e46808df313274776aaf81be1e1
Ivan Ma <ivanxma@yahoo.com>
2022-11-10 08:28:54 +0000

    update

--- 13-InnoDBCluster/newcerts/01-createCA.sh
+++ 13-InnoDBCluster/newcerts/01-createCA.sh
@@ -1,3 +1,2 @@
 openssl genrsa 2048 > template/ca-key.pem
-openssl req -new -x509 -nodes -days 3600 \
-        -key template/ca-key.pem -out template/ca.pem
+openssl req -new -x509 -nodes -days 3600 -key template/ca-key.pem -out template/ca.pem --config template/myopenssl.cnf
