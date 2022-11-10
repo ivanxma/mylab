18f819481b1b1a606ce3d659a57bcbfb25eb8501
Ivan Ma <ivanxma@yahoo.com>
2022-11-10 08:37:14 +0000

    fix

--- 13-InnoDBCluster/newcerts/02-createCert.sh
+++ 13-InnoDBCluster/newcerts/02-createCert.sh
@@ -15,7 +15,7 @@ mkdir $1
 cd $1
 
 cp ../template/* .
-sed  "s/MYHOST/$1/g" template/myopenssl.cnf > myopenssl.cnf
+sed  "s/MYHOST/$1/g" ../template/myopenssl.cnf > myopenssl.cnf
 openssl req -newkey rsa:2048 -days 3600         -nodes -keyout server-key.pem -out server-req.pem -config myopenssl.cnf 
 openssl x509 -req -in ./server-req.pem -days 3600         -CA ca.pem -CAkey ca-key.pem -set_serial 01 -out server-cert.pem
 
