7775e0ad0bb145c2253bdcf6196e943038c394d6
Ivan Ma <ivanxma@yahoo.com>
2022-11-11 01:03:42 +0000

    add serial 01

--- 13-InnoDBCluster/newcerts/01-createCA.sh
+++ 13-InnoDBCluster/newcerts/01-createCA.sh
@@ -1,3 +1,3 @@
 openssl genrsa 2048 > template/ca-key.pem
 sed  "s/MYHOST/$HOSTNAME/g" template/myopenssl.cnf > myopenssl.cnf
-openssl req -new -x509 -nodes -days 3600 -key template/ca-key.pem -out template/ca.pem --config myopenssl.cnf
+openssl req -new -x509 -nodes -days 3600 -set_serial 01 -key template/ca-key.pem -out template/ca.pem --config myopenssl.cnf
