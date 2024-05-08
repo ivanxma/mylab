4054c6c987611d7299cb95f474bac54719515b28
Ivan Ma <ivanxma@yahoo.com>
2024-05-08 10:07:28 +0000

    changed with MySQL 8.4 update

--- 04-MEB/01-setup/03-ChangePassword.sh
+++ 04-MEB/01-setup/03-ChangePassword.sh
@@ -4,6 +4,6 @@ LOGFILE=`grep log-error  config/my1.cnf|cut -f2 -d\=`
 PASS=`grep temporary $LOGFILE |sed 's/localhost: /\~/g'|cut -f2 -d\~`
 
 mysql -uroot -h127.0.0.1 -P3306 -p"$PASS"  --connect-expired-password << EOL
-alter user root@'localhost' identified with 'mysql_native_password' by 'mysql';
+alter user root@'localhost' identified  by 'mysql';
 EOL
 
--- 04-MEB/EncryptedBackup/03-changePassword.sh
+++ 04-MEB/EncryptedBackup/03-changePassword.sh
@@ -3,6 +3,6 @@
 PASS=`grep temporary /home/mysql/data/lab02/my.error |sed 's/localhost: /\~/g'|cut -f2 -d\~`
 
 mysql -uroot -h127.0.0.1 -P3306 -p"$PASS"  --connect-expired-password << EOL
-alter user root@'localhost' identified with 'mysql_native_password' by 'mysql';
+alter user root@'localhost' identified by 'mysql';
 EOL
 
